bc9ff25929573fa1503f2b162f20b605ca03991b
michael920516 <michael_920516@hotmail.com>
2014-10-27 17:57:58 +0800

    add picture and treemap activity

--- gen/com/example/genealogy/R.java
+++ gen/com/example/genealogy/R.java
@@ -9,6 +9,17 @@ package com.example.genealogy;
 
 public final class R {
     public static final class attr {
+        /** <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
+or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
+         */
+        public static final int metaButtonBarButtonStyle=0x7f010001;
+        /** <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
+or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
+         */
+        public static final int metaButtonBarStyle=0x7f010000;
+    }
+    public static final class color {
+        public static final int black_overlay=0x7f040000;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -18,40 +29,43 @@ public final class R {
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_add=0x7f08001a;
-        public static final int action_delete=0x7f080019;
-        public static final int action_edit=0x7f080017;
-        public static final int action_profile=0x7f080016;
-        public static final int action_search=0x7f080018;
-        public static final int action_settings=0x7f080015;
-        public static final int button1=0x7f08000d;
-        public static final int button2=0x7f08000c;
-        public static final int button3=0x7f08000e;
-        public static final int checkBox1=0x7f080006;
-        public static final int editText1=0x7f080008;
-        public static final int editText2=0x7f080007;
-        public static final int editText3=0x7f08000a;
-        public static final int editText4=0x7f080009;
-        public static final int editText5=0x7f08000b;
-        public static final int imageView1=0x7f080005;
-        public static final int imageView2=0x7f080011;
-        public static final int listView1=0x7f080012;
-        public static final int spinner1=0x7f080010;
-        public static final int spinner2=0x7f08000f;
-        public static final int textView1=0x7f080001;
-        public static final int textView2=0x7f080000;
-        public static final int textView3=0x7f080004;
-        public static final int textView4=0x7f080003;
-        public static final int textView5=0x7f080002;
-        public static final int tvCalled=0x7f080014;
-        public static final int tvName=0x7f080013;
+        public static final int action_add=0x7f09001d;
+        public static final int action_delete=0x7f09001c;
+        public static final int action_edit=0x7f09001a;
+        public static final int action_profile=0x7f090019;
+        public static final int action_search=0x7f09001b;
+        public static final int action_settings=0x7f090018;
+        public static final int button1=0x7f09000d;
+        public static final int button2=0x7f09000c;
+        public static final int button3=0x7f09000e;
+        public static final int checkBox1=0x7f090006;
+        public static final int dummy_button=0x7f090015;
+        public static final int editText1=0x7f090008;
+        public static final int editText2=0x7f090007;
+        public static final int editText3=0x7f09000a;
+        public static final int editText4=0x7f090009;
+        public static final int editText5=0x7f09000b;
+        public static final int fullscreen_content=0x7f090013;
+        public static final int fullscreen_content_controls=0x7f090014;
+        public static final int imageView1=0x7f090005;
+        public static final int imageView2=0x7f090011;
+        public static final int listView1=0x7f090012;
+        public static final int spinner1=0x7f090010;
+        public static final int spinner2=0x7f09000f;
+        public static final int textView1=0x7f090001;
+        public static final int textView2=0x7f090000;
+        public static final int textView3=0x7f090004;
+        public static final int textView4=0x7f090003;
+        public static final int textView5=0x7f090002;
+        public static final int tvCalled=0x7f090017;
+        public static final int tvName=0x7f090016;
     }
     public static final class layout {
         public static final int activity_add=0x7f030000;
@@ -59,24 +73,28 @@ public final class R {
         public static final int activity_main=0x7f030002;
         public static final int activity_profile=0x7f030003;
         public static final int activity_second=0x7f030004;
-        public static final int listview=0x7f030005;
+        public static final int activity_tree_map=0x7f030005;
+        public static final int listview=0x7f030006;
     }
     public static final class menu {
-        public static final int add=0x7f070000;
-        public static final int edit=0x7f070001;
-        public static final int main=0x7f070002;
-        public static final int profile=0x7f070003;
-        public static final int second=0x7f070004;
+        public static final int add=0x7f080000;
+        public static final int edit=0x7f080001;
+        public static final int main=0x7f080002;
+        public static final int profile=0x7f080003;
+        public static final int second=0x7f080004;
     }
     public static final class string {
-        public static final int action_profile=0x7f050003;
-        public static final int action_settings=0x7f050002;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
-        public static final int title_activity_add=0x7f050007;
-        public static final int title_activity_edit=0x7f050006;
-        public static final int title_activity_profile=0x7f050005;
-        public static final int title_activity_second=0x7f050004;
+        public static final int action_profile=0x7f060003;
+        public static final int action_settings=0x7f060002;
+        public static final int app_name=0x7f060000;
+        public static final int dummy_button=0x7f060009;
+        public static final int dummy_content=0x7f06000a;
+        public static final int hello_world=0x7f060001;
+        public static final int title_activity_add=0x7f060007;
+        public static final int title_activity_edit=0x7f060006;
+        public static final int title_activity_profile=0x7f060005;
+        public static final int title_activity_second=0x7f060004;
+        public static final int title_activity_tree_map=0x7f060008;
     }
     public static final class style {
         /** 
@@ -100,10 +118,56 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
+        public static final int ButtonBar=0x7f070003;
+        public static final int ButtonBarButton=0x7f070004;
+        public static final int FullscreenActionBarStyle=0x7f070005;
+        public static final int FullscreenTheme=0x7f070002;
     }
+    public static final class styleable {
+        /** 
+         Declare custom theme attributes that allow changing which styles are
+         used for button bars depending on the API level.
+         ?android:attr/buttonBarStyle is new as of API 11 so this is
+         necessary to support previous API levels.
+    
+           <p>Includes the following attributes:</p>
+           <table>
+           <colgroup align="left" />
+           <colgroup align="left" />
+           <tr><th>Attribute</th><th>Description</th></tr>
+           <tr><td><code>{@link #ButtonBarContainerTheme_metaButtonBarButtonStyle com.example.genealogy:metaButtonBarButtonStyle}</code></td><td></td></tr>
+           <tr><td><code>{@link #ButtonBarContainerTheme_metaButtonBarStyle com.example.genealogy:metaButtonBarStyle}</code></td><td></td></tr>
+           </table>
+           @see #ButtonBarContainerTheme_metaButtonBarButtonStyle
+           @see #ButtonBarContainerTheme_metaButtonBarStyle
+         */
+        public static final int[] ButtonBarContainerTheme = {
+            0x7f010000, 0x7f010001
+        };
+        /**
+          <p>This symbol is the offset where the {@link com.example.genealogy.R.attr#metaButtonBarButtonStyle}
+          attribute's value can be found in the {@link #ButtonBarContainerTheme} array.
+
+
+          <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
+or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
+          @attr name com.example.genealogy:metaButtonBarButtonStyle
+        */
+        public static final int ButtonBarContainerTheme_metaButtonBarButtonStyle = 1;
+        /**
+          <p>This symbol is the offset where the {@link com.example.genealogy.R.attr#metaButtonBarStyle}
+          attribute's value can be found in the {@link #ButtonBarContainerTheme} array.
+
+
+          <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
+or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
+          @attr name com.example.genealogy:metaButtonBarStyle
+        */
+        public static final int ButtonBarContainerTheme_metaButtonBarStyle = 0;
+    };
 }
