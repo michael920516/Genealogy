8791ab71e89d1eddda6826fd0b568ce3ceb462e3
michael920516 <michael_920516@hotmail.com>
2014-10-29 17:27:03 +0800

    MapActivity add button and textview

--- gen/com/example/genealogy/R.java
+++ gen/com/example/genealogy/R.java
@@ -38,38 +38,39 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int relativesmap=0x7f020002;
     }
     public static final class id {
-        public static final int action_add=0x7f09001f;
-        public static final int action_delete=0x7f09001e;
-        public static final int action_edit=0x7f09001c;
-        public static final int action_map=0x7f09001a;
-        public static final int action_profile=0x7f09001b;
-        public static final int action_search=0x7f09001d;
-        public static final int action_settings=0x7f090019;
+        public static final int action_add=0x7f090020;
+        public static final int action_delete=0x7f09001f;
+        public static final int action_edit=0x7f09001d;
+        public static final int action_map=0x7f09001b;
+        public static final int action_profile=0x7f09001c;
+        public static final int action_search=0x7f09001e;
+        public static final int action_settings=0x7f09001a;
         public static final int button1=0x7f09000c;
-        public static final int button111=0x7f090011;
-        public static final int button112=0x7f090012;
+        public static final int button111=0x7f090012;
+        public static final int button112=0x7f090013;
         public static final int button2=0x7f09000b;
         public static final int button3=0x7f09000d;
         public static final int checkBox1=0x7f090006;
         public static final int editText1=0x7f090007;
-        public static final int editText2=0x7f090015;
+        public static final int editText2=0x7f090016;
         public static final int editText3=0x7f090009;
         public static final int editText4=0x7f090008;
         public static final int editText5=0x7f09000a;
         public static final int imageView1=0x7f090005;
         public static final int imageView2=0x7f090010;
-        public static final int listView1=0x7f090016;
+        public static final int listView1=0x7f090017;
+        public static final int rLayout=0x7f090011;
         public static final int spinner1=0x7f09000f;
         public static final int spinner2=0x7f09000e;
         public static final int textView111=0x7f090001;
-        public static final int textView112=0x7f090013;
-        public static final int textView113=0x7f090014;
+        public static final int textView112=0x7f090014;
+        public static final int textView113=0x7f090015;
         public static final int textView2=0x7f090000;
         public static final int textView3=0x7f090004;
         public static final int textView4=0x7f090003;
         public static final int textView5=0x7f090002;
-        public static final int tvCalled=0x7f090018;
-        public static final int tvName=0x7f090017;
+        public static final int tvCalled=0x7f090019;
+        public static final int tvName=0x7f090018;
     }
     public static final class layout {
         public static final int activity_add=0x7f030000;
--- src/com/example/genealogy/MapActivity.java
+++ src/com/example/genealogy/MapActivity.java
@@ -5,16 +5,23 @@ import java.util.ArrayList;
 import com.example.genealogy.data.Member;
 import com.example.genealogy.data.MemberDAODBImpl;
 import android.app.Activity;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup.LayoutParams;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.Spinner;
+import android.widget.TableLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -25,14 +32,18 @@ public class MapActivity extends Activity {
 	Button bt311,bt312,bt321,bt322,bt331,bt332,bt341,bt342,bt351,bt352,bt361,bt362,bt371,bt372;
 	Button bt411,bt412,bt421,bt422,bt431,bt432,bt441,bt442;
 	Button bt511,bt512,bt521,bt522;
-	TextView tv111,tv112,tv113;
+	TextView tv1111,tv1112,tv1113,tv1121,tv1122,tv1123,mType,mValue;
 	int Serial;
 	MemberDAODBImpl dao;
 	Member [] data;
 	ArrayAdapter<String> adapter11,adapter12;
 	ArrayList<Boolean> listShow;
 	ArrayList<String> getItem;
+	String [] mTitle = {"tv111","tv112","tv121",
+						"tv122","tv211","tv212"};
 	Spinner sll11;
+	View relativeLayout;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -45,41 +56,144 @@ public class MapActivity extends Activity {
         bt112 = (Button) findViewById(R.id.button112);
 //        bt121 = (Button) findViewById(R.id.button121);
 //        bt122 = (Button) findViewById(R.id.button122);
-        tv111 = (TextView) findViewById(R.id.textView111);
-        tv112 = (TextView) findViewById(R.id.textView112);
-        tv113 = (TextView) findViewById(R.id.textView113);
+        
+//        tv1111 = (TextView) findViewById(R.id.textView1111);
+//        tv1112 = (TextView) findViewById(R.id.textView1112);
+//        tv1113 = (TextView) findViewById(R.id.textView1113);
+
+        // add TextView  
+
+        //end add TextView
+        
+        
         listShow = new ArrayList();
         getItem = new ArrayList();
        
-	    dao = new MemberDAODBImpl(this);	            
-        bt111.setOnClickListener(new Button.OnClickListener(){  	
+       
+	    dao = new MemberDAODBImpl(this);	
+	//first level button
+	    MakeBT(25,55,0);   
+	    MakeBT(48,55,1);
+	    MakeBT(331,55,2);
+	    MakeBT(350,55,3);
+	//Second level button
+	    MakeBT(6,190,4);
+	    MakeBT(25,190,5);
+	    MakeBT(60,190,6);
+	    MakeBT(79,190,7);
+	    MakeBT(180,190,8);
+	    MakeBT(199,190,9);
+	    MakeBT(300,190,10);
+	    MakeBT(319,190,11);
+	    MakeBT(370,190,12);
+	    MakeBT(389,190,13);
+	 //Third level button
+	    MakeBT(6,325,14);
+	    MakeBT(25,325,15);
+	    MakeBT(60,325,16);
+	    MakeBT(79,325,17);
+	    MakeBT(115,325,18);
+	    MakeBT(134,325,19);	    	    
+	    MakeBT(180,325,20);
+	    MakeBT(199,325,21);
+	    MakeBT(250,325,22);
+	    MakeBT(269,325,23);
+	    MakeBT(321,325,24);
+	    MakeBT(340,325,25);	    
+	    MakeBT(370,325,26);
+	    MakeBT(389,325,27);
+	 //Forth level button
+	    MakeBT(115,460,28);
+	    MakeBT(134,460,29);	    	    
+	    MakeBT(161,460,30);
+	    MakeBT(180,460,31);
+	    MakeBT(200,460,32);
+	    MakeBT(219,460,33);
+	    MakeBT(250,460,34);
+	    MakeBT(269,460,35);
+	 //Fifth level button
+	    MakeBT(161,610,36);
+	    MakeBT(180,610,37);
+	    MakeBT(200,610,38);
+	    MakeBT(219,610,39);    
+	}
+    void MakeTV(int left,int top,int right,int bottom) {
+    	relativeLayout = findViewById(R.id.rLayout);
+        mType = new TextView(MapActivity.this);
+        mType.setLayoutParams(new TableLayout.LayoutParams(
+        		LayoutParams.WRAP_CONTENT,
+        		LayoutParams.WRAP_CONTENT,1f));
+        mType.setPadding(left, top, right, bottom);
+        mType.setTextSize(18);
+        mType.setTypeface(Typeface.DEFAULT_BOLD);
+//        mType.setGravity(Gravity.LEFT | Gravity.CENTER);
+//        mType.setText(t);
+        ((RelativeLayout) relativeLayout).addView(mType);  
+    }
+    void MakeBT(int left,int top, int i) {
+    	relativeLayout = findViewById(R.id.rLayout);
+        Button bt = new Button(this);
+        bt.setLayoutParams(new LayoutParams(25,100));     
+//        bt.setText("Button " + i);
+        bt.setId(i);
+//        bt.setBackgroundColor(5000);
+        ((RelativeLayout) relativeLayout).addView(bt);  
+        RelativeLayout.LayoutParams bt2=(RelativeLayout.LayoutParams)bt.getLayoutParams();
+        bt2.leftMargin=left;//your left margin here
+        bt2.topMargin=top;//you       
+        ((Button)findViewById(i)).setOnClickListener(new OnClickListener(){
 			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub	
-				String CallTable = "兒子"; 
-//				Toast.makeText(MapActivity.this, CallTable, Toast.LENGTH_SHORT).show();
-			            data = dao.searchCall(CallTable);
-//			            Toast.makeText(MapActivity.this, data.length, Toast.LENGTH_SHORT).show();
-			            for(int i=0;i<data.length;i++) {
-			            	int n = i+111; 
-			            	getItem.add(data[i].Name);
-			            	switch (n) {
-			            	case 111 :
-			            		tv111.setText(data[i].Name+"");
-			            		break;
-			            	case 112 :
-			            		tv112.setText(data[i].Name+"");
-			            		break;
-			            	case 113 :
-			            		tv113.setText(data[i].Name+"");
-			            		break;
-			            	}
-			            	Toast.makeText(MapActivity.this, data[i].Name + "" , Toast.LENGTH_SHORT).show();
-			            }   
-		}});
-        
- 	}
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				String CallTable = "祖父"; 
+	            data = dao.searchCall(CallTable);
+	            if(data.length > 0) {
+	            	for(int i=0;i<data.length;i++) {
+		            	int n = i+1;		            
+		            	getItem.add(data[i].Name);
+		            	switch (n) {
+		            	case 1 :
+		            		MakeTV( 150, 120, 130, 120 );
+		            		mType.setText(data[i].Name+"");
+		            		break;
+		            	case 2 :
+		            		MakeTV( 150, 140, 130, 120 );
+		            		mType.setText(data[i].Name+"");
+		            		break;
+		            	case 3 :
+		            		MakeTV( 150, 150, 130, 120 );
+		            		mType.setText(data[i].Name+"");
+		            		break;
+		            	}
+	            	}  	
+	            }else {
+	            	MakeTV( 150, 120, 130, 120 );
+	            	mType.setText("無資料");     
+	            }            
+			}});
 
+    }
+    void delay(int d) {
+	    final int welcomeScreenDisplay = d;
+	    /** create a thread to show splash up to splash time */
+	    Thread welcomeThread = new Thread() {
+	        int wait = 0;
+	        @Override
+	        public void run() {
+	            try {
+	                super.run();
+	                while (wait < welcomeScreenDisplay) {
+	                    sleep(100);
+	                    wait += 100;
+	                }
+	            } catch (Exception e) {
+	                Log.d("mike", "EXc=" + e);
+	            }      
+	        }    
+	    };
+	    welcomeThread.start();
+    }
+   
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
